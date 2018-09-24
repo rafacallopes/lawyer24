b3fdacd8f03a9fa7cdf451a2abd4a40247c270dd
rafa <rafa@rafas-MacBook-Pro.local>
2018-09-24 13:53:37 -0300

    card v1 added. Not rendering on the page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,10 +4,40 @@ import {
   Text,
   View
 } from 'react-native';
-import {Header, Container, Title, Button, Icon, Left, Right} from 'native-base';
+import {Card, Button} from 'react-native-elements';
+import {Header, Container, Title, Icon, Left, Right} from 'native-base';
 import { Actions } from 'react-native-router-flux'; // New code
+import Deck from './Deck'
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
 class Main extends Component {
+
+  renderCard = (item) => {
+  <Card
+    title={item.text}
+    image={{ uri: item.uri }}
+  >
+  <Text style = {{marginBottom: 10 }}>Hello World!</Text>
+  <Button
+    icon={{ name: 'code'}}
+    backgroundColor='#03A9F4'
+    title="View Now"
+  />
+  
+  </Card>
+  }
+
+
 render(){
   return (
     <Container>
@@ -22,6 +52,14 @@ render(){
       </Header>
 
   
+    <View style={styles.container}>
+        <Deck 
+        data={DATA}
+        renderCard={this.renderCard}
+        />
+    </View>
+
+
 
     </Container>
   );
@@ -31,9 +69,7 @@ render(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#bb0000',
+    
   },
   welcome: {
     fontSize: 20,
